19c46999bb77e635fcf2010223857511251b9ddf
廖伟 <liaowei@liaoweideMacBook-Pro.local>
2018-02-13 10:18:03 +0800

    require路径bug修复

---
+++ test/js/require.js
@@ -0,0 +1,18 @@
+var jsToAlipay = require('../../lib/js.ast')
+var assert = require('assert')
+
+describe('require', function() {
+  it('纯名字', () => {
+    let code = `const dateFormat = require('dateFormat')`
+    let result = jsToAlipay(code)
+
+    assert.equal(result, `var dateFormat = require("./dateFormat");`);
+  })
+
+  it('带路径', () => {
+    let code = `var _babelFn = require('polyfill/index')`
+    let result = jsToAlipay(code)
+
+    assert.equal(result, `var _babelFn = require("./polyfill/index");`);
+  })
+})
